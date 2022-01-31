03aafbba0a11df4dc923f1b372df9f888165171b
Matt <matt.d.moore@outlook.com>
2022-01-31 16:50:13 +0100

    Simulates on real target words

--- src/functions/n_guesses.R
+++ src/functions/n_guesses.R
@@ -1,10 +1,10 @@
 # How many guesses between specified or random word and target
-n_guesses = function(word_list, word=NULL, target=NULL)
+n_guesses = function(word_list, word=NULL, target=NULL, solutions_list)
 {
-  random_word = function(word_list) word_list[sample(n[1], 1),]
+  random_word = function(word_list) word_list[sample(nrow(word_list), 1),]
   
-  if(is.null(word)) word = random_word(word_list)
-  if(is.null(target)) target = random_word(word_list)
+  if(is.null(word)) word = random_word(word_list) %>% unlist
+  if(is.null(target)) target = random_word(solutions_list)
   
   source('src/functions/find_target.R')
   out = find_target(word, target, word_list, p=F)
