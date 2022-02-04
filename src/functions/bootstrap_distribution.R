b1b429b35d0b38d1bb492fddccc5307980d87ff1
Matt <matt.d.moore@outlook.com>
2022-02-04 11:41:27 +0100

    Added checkpoint saves to bootstrap

--- src/functions/bootstrap_distribution.R
+++ src/functions/bootstrap_distribution.R
@@ -1,14 +1,17 @@
+library('magrittr')
+library('tidyverse')
 bootstrap_distribution = function(word_list=NULL, 
                                   R=NULL) 
 {
   data_dir = 'data'
   files = list.files(data_dir)
-  
+  Rx = 0
   y = NULL
   if(any(grepl('bootstrap', files))) {
     idx = grepl('bootstrap', files) %>% which
     file = paste(data_dir, files[idx], sep = '/')
     load(file)
+    Rx = length(y)
   } 
   
   # Generate new bootstrap distribution, 
@@ -21,14 +24,19 @@ bootstrap_distribution = function(word_list=NULL,
     cat('Generating bootstrap distribution with', R, 'repetitions\n')
     for(i in 1:R)
     {
-      y[i] = n_guesses(words$split)
+      y[i] = n_guesses(words$split, solutions_list = solutions$split)
       
       # Real-time progress monitoring
       cat(sprintf('\rMean: %.2f\tStDev: %.2f\tFail rate: %.02f%%\tN: %d/%d', 
                   mean(y[y > 0]), 
                   sd(y[y > 0]),
-                  100*length(which(y > 6))/i,
-                  i,R))
+                  100 * length(which(y > 6)) / i,
+                  i + Rx,
+                  R + Rx))
+      
+      # Save checkpoint every 500 simulations
+      checkpoint = y[y > 0]
+      if((i %% 500) == 0) save(checkpoint, file='data/bootstrap.RData')
     }
   }
   
